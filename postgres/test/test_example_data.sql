6a9545bf5b79a18e18dc787269ad17630baf33f9
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-08-19 13:53:38 +0100

    update test values

--- postgres/test/test_example_data.sql
+++ postgres/test/test_example_data.sql
@@ -1,3 +1,7 @@
 INSERT INTO ACCESS (ID, SHORTCODE, CANPRINT, CANLASERCUT, VALID) VALUES
 ('12345678', 'abc123', 'TRUE', 'TRUE', 'TRUE'),
 ('AB345678', 'xyz123', 'FALSE', 'FALSE', 'FALSE');
+
+INSERT INTO INDUCTION_QUIZ (QUESTION, CORRECT_OPTIONS, INCORRECT_OPTIONS, NUM_ANSWERS) VALUES
+    ('The color of the sky is?', 'blue', 'green;red;yellow', 1),
+    ('Which of the following are vehicles', 'car;truck;bike', 'building;river', 3);
